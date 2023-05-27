65cb2f5a6c61b92282d1d69b4968b92f038cb2bc
Ruixi Lin <ruixi@u.nus.edu>
2023-05-27 18:57:30 +0800

    Update example.sh

--- prompt_based_finetuning/example.sh
+++ prompt_based_finetuning/example.sh
@@ -4,13 +4,13 @@ export PYTHONPATH=$ADAPET_ROOT:$PYTHONPATH
 export PYTHON_EXEC=python
 
 
-####### Example: run prompt-based fine-tuning experiments on Biomegatron cased #######
+####### Example: run prompt-based fine-tuning experiments on GatorTron #######
 # Please specify your data path to train on full train set or subsets, e.g., data/medical/train_{} (full, 100, 500, 1000, 5000)
-# Place your BioMegatron model files in biomegatronModel
+# Place your GatorTron model files in gatortronModel
 CUDA_VISIBLE_DEVICES=1 nohup python cli.py -d data/medical \
               -p '[TEXT1] [SEP] In summary, this is a [LBL]' \
               -v '{"0": "no evidence of disease", "1": "partial response", "2": "stable disease", "3": "progressive disease"}' \
-              -w biomegatronModel \
+              -w gatortronModel \
               -bs 1 \
               --grad_accumulation_factor 16 \
               --num_batches 2000 \
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=1 nohup python cli.py -d data/medical \
 
 
 # [EXP_FOLDER] is the trained model folder name, e.g., 2023-01-26-13-26-43, then get test preds
-sh bin/test.sh biomegatronModel/[EXP_FOLDER]
+sh bin/test.sh gatortronModel/[EXP_FOLDER]
 # Evaluate on test preds
-python get_test_scores_medical.py -y data/medical -pred biomegatronModel/[EXP_FOLDER]
+python get_test_scores_medical.py -y data/medical -pred gatortronModel/[EXP_FOLDER]
 

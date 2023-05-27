7667669dcdf346c4949444bd1ed72fad30f157df
Ruixi Lin <ruixi@u.nus.edu>
2023-05-27 14:31:10 +0800

    Update example.sh

--- prompt_based_finetuning/example.sh
+++ prompt_based_finetuning/example.sh
@@ -3,15 +3,13 @@ export ADAPET_ROOT=`pwd`
 export PYTHONPATH=$ADAPET_ROOT:$PYTHONPATH
 export PYTHON_EXEC=python
 
-HOME_DIR=/your/adapet/home
-cd $HOME_DIR
 
 ####### Example: run prompt-based fine-tuning experiments on Biomegatron cased #######
-# Train on full train set, change to $HOME_DIR/data/medical/train_{} (100, 500, 1000, 5000) for subset experiemnts 
-CUDA_VISIBLE_DEVICES=1 nohup python $HOME_DIR/cli.py -d $HOME_DIR/data/medical \
+# Train on full train set, change to data/medical/train_{} (100, 500, 1000, 5000) for subset experiemnts 
+CUDA_VISIBLE_DEVICES=1 nohup python cli.py -d data/medical \
               -p '[TEXT1] [SEP] In summary, this is a [LBL]' \
               -v '{"0": "complete response", "1": "stable disease", "2": "progressive disease", "3": "partial response"}' \
-              -w $HOME_DIR/biomegatronModel \
+              -w biomegatronModel \
               -bs 1 \
               --grad_accumulation_factor 16 \
               --num_batches 2000 \
@@ -24,7 +22,7 @@ CUDA_VISIBLE_DEVICES=1 nohup python $HOME_DIR/cli.py -d $HOME_DIR/data/medical \
 
 
 # Get test preds, put the model folder name in [EXP_FOLDER], e.g., 2023-01-26-13-26-43
-sh bin/test.sh /home/rlin/bio/ADAPET-copy/biomegatron345mcased/[EXP_FOLDER]
+sh bin/test.sh biomegatron345mcased/[EXP_FOLDER]
 # Eval test
-python get_test_scores_medical.py -y /home/rlin/bio/ADAPET-copy/data/medical_updated -pred /home/rlin/bio/ADAPET-copy/biomegatron345mcased/[EXP_FOLDER]
+python get_test_scores_medical.py -y data/medical -pred biomegatron345mcased/[EXP_FOLDER]
 
